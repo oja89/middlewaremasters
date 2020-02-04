b847586fbccdb122e8d7b37f959526e09cc00c19
Markus <marquezo89@gmail.com>
2020-02-04 12:49:16 +0200

    Fixed comment

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 //making this work with chrome first
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript({
-      file: 'scripts\toggle.js' //cant get this to work
+      file: 'scripts\toggle.js' //works now, added function to toggle.js
       //code: 'var video = document.getElementsByTagName("video")[0]; if (video.paused == true) {video.play(); } else {video.pause()}'
     });
   });
