a973d100342c9ccab36610b28cfdc2ed15d3ad24
Markus <marquezo89@gmail.com>
2020-02-04 15:48:35 +0200

    Converted to message-listener

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
 //making this work with chrome first
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript({
-      file: 'scripts\toggle.js' //works now, added function to toggle.js
-      //code: 'var video = document.getElementsByTagName("video")[0]; if (video.paused == true) {video.play(); } else {video.pause()}'
-    });
-  });
+  let message = "toggle"; 
+  //conffed messages: "toggle"
+
+  chrome.tabs.query({active: true,currentWindow:true},function(tabs){
+    chrome.tabs.sendMessage(tabs[0].id,message);
+  })
+})
+
--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,9 +1,14 @@
-(function() {
+chrome.runtime.onMessage.addListener(call);
+  //name doesn't matter?
+function call(message, sender, sendresponse) {
     var video = document.getElementsByTagName("video")[0];
-    if (video.paused == true) {
-        video.play();
+    if (message == "toggle") {
+        if (video.paused == true) {
+            video.play();
+        }
+        else {
+            video.pause()
+        }
     }
-    else {
-        video.pause()
-    }
-})();
+}
+
