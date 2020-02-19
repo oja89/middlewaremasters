afeddb15feb9d5003f03a59aeb549e1a43b0e8f9
Markus <marquezo89@gmail.com>
2020-02-19 12:28:43 +0200

    Messaging between popup and status

---
+++ popup/popup.js
@@ -0,0 +1,11 @@
+function hello() {
+    
+      //console.log("hello")
+      let message = "status"
+      chrome.tabs.query({active: true,currentWindow:true},function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id,message);
+        console.log("sent: "+ message)
+      })
+}
+  
+  document.getElementById('play').addEventListener('click', hello);
