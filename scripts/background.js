fb4b6eb98742fde3d837c38353ac40d4a09f4e73
Markus <marquezo89@gmail.com>
2020-02-21 17:11:51 +0200

    Removing unnecessary splitting of functions

--- scripts/background.js
+++ scripts/background.js
@@ -6,13 +6,9 @@ function rollStatus() {
   chrome.tabs.query({active: true,currentWindow:true},function(tabs){
     chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
       console.log(response) //this is the status-object
-      status = response; //this was just something to get this forward
     });
-    console.log("sent: "+ message)
   })
 }
 
 //intervaltime in ms
-const createClock = setInterval(rollStatus, 6000);
-
-//all previous stuff with the button testing is now irrelevant, the popup overrides the button function
+const createClock = setInterval(rollStatus, 1000);
